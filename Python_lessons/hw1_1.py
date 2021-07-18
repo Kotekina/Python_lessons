0e5ad0f2b1b5f71d9c020ac1baf3ff493092e863
Svetlana <kotekina.sv@gmail.com>
2021-07-18 14:33:15 +0300

    Initial commit

---
+++ Python_lessons/hw1_1.py
@@ -0,0 +1,17 @@
+name = "Sveta"
+age = 35
+data =17.6
+Day = True
+Night = None
+
+print(type(name))
+print(type(age))
+print(type(data))
+print(type(Day))
+print(type(Night))
+
+# запрос у пользователя числа
+x = int(input('Введите любое число:'))
+y = str(input("Введите адрес проживания"))
+print(x, y)
+
